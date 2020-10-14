7abf26cfa03ae9f3d87d96e63373d62ad47b1ac1
Phileas Fogg <phileas.fogg@mail.com>
2020-10-14 15:15:34 +0200

    Ripple Carry Add works under GHDL (No generic parameters)

--- RippleRarryAdder/ripple_carry_adder.vhd
+++ RippleRarryAdder/ripple_carry_adder.vhd
@@ -2,25 +2,47 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
  
-entity full_adder is
+entity ripple_carry_adder is
     port (
-        i_a  : in std_logic;
-        i_b  : in std_logic;
-        i_carry : in std_logic;
-        o_sum   : out std_logic;
-        o_carry : out std_logic
+        i_add_term1  : in std_logic_vector(1 downto 0);
+        i_add_term2  : in std_logic_vector(1 downto 0);
+        o_result   : out std_logic_vector(2 downto 0)
     );
-end full_adder;
+end ripple_carry_adder;
  
-architecture rtl of full_adder is
-    signal w_wire_1 : std_logic;
-    signal w_wire_2 : std_logic;
-    signal w_wire_3 : std_logic;
-begin
-    w_wire_1 <= i_a xor i_b;
-    w_wire_2 <= w_wire_1 and i_carry;
-    w_wire_3 <= i_a and i_b;
+architecture rtl of ripple_carry_adder is
+    component full_adder is
+        port (
+            i_a     : in  std_logic;
+            i_b     : in  std_logic;
+            i_carry : in  std_logic;
+            o_sum   : out std_logic;
+            o_carry : out std_logic
+        );
+    end component full_adder;
 
-    o_sum   <= w_wire_1 xor i_carry;
-    o_carry <= w_wire_2 or w_wire_3;
+    signal w_carry : std_logic_vector(2 downto 0);
+    signal w_sum   : std_logic_vector(1 downto 0);
+begin
+    w_carry(0) <= '0';
+
+    FULL_ADDER_1_INST : full_adder
+        port map (
+            i_a  => i_add_term1(0),
+            i_b  => i_add_term2(0),
+            i_carry => w_carry(0),
+            o_sum   => w_sum(0),
+            o_carry => w_carry(1)
+        );
+
+    FULL_ADDER_2_INST : full_adder
+        port map (
+            i_a  => i_add_term1(1),
+            i_b  => i_add_term2(1),
+            i_carry => w_carry(1),
+            o_sum   => w_sum(1),
+            o_carry => w_carry(2)
+        );
+
+    o_result <= w_carry(2) & w_sum;
 end rtl;
--- RippleRarryAdder/ripple_carry_adder_tb.vhd
+++ RippleRarryAdder/ripple_carry_adder_tb.vhd
@@ -2,61 +2,54 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity full_adder_tb is
-end full_adder_tb;
+entity ripple_carry_adder_tb is
+end ripple_carry_adder_tb;
 
-architecture behave of full_adder_tb is
-    signal r_a  : std_logic := '0';
-    signal r_b  : std_logic := '0';
-    signal w_sum   : std_logic;
-    signal r_carry : std_logic := '0';
-    signal w_carry : std_logic;
+architecture behave of ripple_carry_adder_tb is
+    constant c_WIDTH : integer := 2;
+
+    signal r_ADD_1  : std_logic_vector(c_WIDTH-1 downto 0) := (others => '0');
+    signal r_ADD_2  : std_logic_vector(c_WIDTH-1 downto 0) := (others => '0');
+    signal w_RESULT : std_logic_vector(c_WIDTH downto 0);
+
+    component ripple_carry_adder is
+        --generic (
+        --    g_WIDTH : natural
+        --);
+        port (
+            i_add_term1 : in  std_logic_vector(c_WIDTH-1 downto 0);
+            i_add_term2 : in  std_logic_vector(c_WIDTH-1 downto 0);
+            o_result    : out std_logic_vector(c_WIDTH downto 0)
+        );
+    end component ripple_carry_adder;
+
 begin
 
-    UUT : entity work.full_adder
+    UUT : ripple_carry_adder
+    --generic map (
+     -- g_WIDTH     => c_WIDTH
+     -- )
     port map (
-        i_a  => r_a,
-        i_b  => r_b,
-        i_carry => r_carry,
-        o_sum   => w_sum,
-        o_carry => w_carry
-    );
-
-    process is
-    begin
-        r_a <= '0';
-        r_b <= '0';
-        r_carry <= '0';
-        wait for 10 ns;
-        r_a <= '0';
-        r_b <= '1';
-        r_carry <= '0';
-        wait for 10 ns;
-        r_a <= '1';
-        r_b <= '0';
-        r_carry <= '0';
-        wait for 10 ns;
-        r_a <= '1';
-        r_b <= '1';
-        r_carry <= '0';
-        wait for 10 ns;
-        r_a <= '0';
-        r_b <= '0';
-        r_carry <= '1';
-        wait for 10 ns;
-        r_a <= '0';
-        r_b <= '1';
-        r_carry <= '1';
-        wait for 10 ns;
-        r_a <= '1';
-        r_b <= '0';
-        r_carry <= '1';
-        wait for 10 ns;
-        r_a <= '1';
-        r_b <= '1';
-        r_carry <= '1';
-        wait for 10 ns;
-        wait for 10 ns;
-        assert FALSE Report "Simulation Done" severity FAILURE;
-    end process;
+      i_add_term1 => r_ADD_1,
+      i_add_term2 => r_ADD_2,
+      o_result    => w_RESULT
+      );
+   
+  process is
+  begin
+    r_ADD_1 <= "00";
+    r_ADD_2 <= "01";
+    wait for 10 ns;
+    r_ADD_1 <= "10";
+    r_ADD_2 <= "01";
+    wait for 10 ns;
+    r_ADD_1 <= "01";
+    r_ADD_2 <= "11";
+    wait for 10 ns;
+    r_ADD_1 <= "11";
+    r_ADD_2 <= "11";
+    wait for 10 ns;
+    wait for 10 ns;
+    assert FALSE Report "Simulation Done" severity FAILURE;
+  end process;
 end behave;
