6e07d378e6f06f5a6d130b9cf00ff10927edb1e9
Phileas Fogg <phileas.fogg@mail.com>
2020-11-20 00:29:44 +0100

    WIP parsing verilog

--- parsed_file.py
+++ parsed_file.py
@@ -12,8 +12,8 @@ class ParsedFile:
     def setVHDLFile(self, filename):
         self.vhdlFile = filename
     def parse(self):
-        print("[.] Parsing %s..." % self.vhdlFile)
-        print("[.] Writing to %s..." % self.hppFile)
+        print("[.] Parsing %s ..." % self.vhdlFile)
+        print("[.] Writing to %s ..." % self.hppFile)
         if self.langage == "VHDL":
             self.parseVHDL()
         elif self.langage == "Verilog":
@@ -28,13 +28,64 @@ class ParsedFile:
         print("[.] Parsing Verilog...")
         state = ""
         with open(self.vhdlFile, "r") as vhdfile:
+            line_count = -1
             for line in vhdfile:
+                # Insert ' ' after and before special characters
+                buff = ""
+                for i in range(len(line)):
+                    flag = False
+                    if line[i] in ['(', ')', '[', ']', '{', '}', '&', '^', '|', '=', ':', ';', '<', '>', '+', '-', ',']:
+                        flag = True
+                    if flag:
+                        buff += " "
+                    buff += line[i]
+                    if flag:
+                        buff += " "
+                line = buff
+                print(line)
+                line_count += 1
                 splited = line.split(" ")
-                for word in splitted:
+                self.data["ignored"] = []
+                self.data["tmp_signal"] = []
+                for word in splited:
                     if state == "module name":
                         # TODO: check if name is valid
-                        data["module"] = word
+                        self.data["module"] = word
                         state = "module wait ("
+                    elif state == "module wait (":
+                        if word == "(":
+                            state = "module declaration"
+                    elif state == "module declaration":
+                        if word == ")":
+                            state = "module wait ;"
+                            # TODO: save signal if needed
+                        if len(self.data["tmp_signal"]) == 0:
+                            if word == "input" or word == "output":
+                                if "input" in self.data["tmp_signal"] or "output" in self.data["tmp_signal"]:
+                                    print("Error ",self.vhdlFile,":",line_count," : input/output has already been set for this signal")
+                                self.data["tmp_signal"].append(word)
+                            elif word == "wire" or word == "reg":
+                                if "wire" in self.data["tmp_signal"] or "reg" in self.data["tmp_signal"]:
+                                    print("Error ",self.vhdlFile,":",line_count," : wire/reg has already been set for this signal")
+                                self.data["tmp_signal"].append(word)
+                            elif word == ",":
+                                # TODO: save signal if needed
+                                pass
+                            elif not "signal name" in self.data:
+                                self.data["signal name"] = word
+                    elif state == "module wait ;":
+                        if word == ";":
+                            state = "module body"
+                        else:
+                            print("Error ",self.vhdlFile,":",line_count," : Expected ';' after module's declaration")
+                    elif state == "module body":
+                        pass
+                        # TODO
                     elif word == "module":
                         state = "module name"
+                    else:
+                        if state == "module wait (":
+                            print("Error ",self.vhdlFile,":",line_count," : Expected '(' after module's name")
+                        self.data["ignored"].append(splited)
+        print(self.data)
 
